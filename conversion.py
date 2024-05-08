1bec3d0af41f25a44e6b7307af7aa6c2b929a4ac
DSS93 <36404391+DSS93@users.noreply.github.com>
2024-05-08 12:10:09 +0530

    Update conversion.py
    Added description

--- conversion.py
+++ conversion.py
@@ -1,6 +1,15 @@
-import datetime
-import pytz
-tzinput=input('Enter TZ :')
+# Importing necessary libraries
+import datetime  # Module for handling dates and times
+import pytz  # Module for handling time zones
+
+# Asking user to input a time zone abbreviation
+tzinput = input('Enter TZ :')
+
+# Getting the current time in the specified time zone
 ustime = datetime.datetime.now(pytz.timezone(tzinput))
-a=ustime.strftime("%Y-%m-%d %H:%M:%S")
+
+# Formatting the current time to display in a specific format
+a = ustime.strftime("%Y-%m-%d %H:%M:%S")
+
+# Printing the formatted current time
 print(a)
